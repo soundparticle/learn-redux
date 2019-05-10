ec000af449da5f8ed83ccde04f59156a6b09ae40
Antreo Pukay <acrosswalker@yahoo.com>
2019-05-09 18:27:09 -0700

    added actionCreators

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,25 @@
+// increment
+// index is which photo tie is incremented
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+// add comment
+export function addComment(postID, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postID,
+    author,
+    comment
+  }
+}
+// remove comment
+export function removeComment(postID, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
