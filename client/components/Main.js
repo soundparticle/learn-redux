e494f3c756a92dd464630ced74e5698241402f05
Antreo Pukay <acrosswalker@yahoo.com>
2019-10-17 21:43:05 -0700

    added App.js file for mapping state and dispatch to props from the store

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-import Single from './Single';
+// import Single from './Single';
 
 const Main = React.createClass({
   render() {
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single'
 import PhotoGrid from './components/PhotoGrid'
 
@@ -15,7 +15,7 @@ import store, { history } from './store';
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute> 
         <Route path="/view/:postId" component={Single}></Route>
       </Route>
