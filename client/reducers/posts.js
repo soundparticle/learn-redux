c2066e7e929ee0e0159c817481ce433749db8507
Antreo Pukay <acrosswalker@yahoo.com>
2019-10-30 21:39:57 -0700

    coded posts reducer and no INCREMENT_LIKES works

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,12 +1,21 @@
+// import { state } from "fs";
+
 // reducer takes in two things:
 // 1. the action (info about what happened)
 // 2. copy of current state 
 
 function posts(state =[], action) {
-  console.log('---The post will change---');
-  console.log('*POSTS', state, action);
-  
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      const i =  action.index;
+      return [
+        ...state.slice(0, i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1),
+      ];
+      default:
+        return state;
+  }
 }
 
 export default posts;
