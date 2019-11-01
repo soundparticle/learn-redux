d34a7477fb72e113913edc98ce2dd0ed3221e28e
Antreo Pukay <acrosswalker@yahoo.com>
2019-10-31 21:49:52 -0700

    created and added Comments component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    console.log('* Index', i);
+    console.log('** Post Data', post);
+
     return (
       <div className="single-photo">
-      I am Single Photo
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>  
-    )
+    );
   }
-})
+});
 
 export default Single;
