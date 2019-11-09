5d46dea0e95f2257ac4a0f05f63b5a349a79d811
Antreo Pukay <acrosswalker@yahoo.com>
2019-11-08 19:04:23 -0800

    added handleSubmit and passed params to Comments. Now its getting the postId

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,27 +1,32 @@
 import React from 'react';
 
 const Comments = React.createClass({
-
   renderComment(comment, i) {
-      console.log(comment);
-      return (
-        <div className="comment" key={i}>
-          <p>
-            <strong>{comment.user}</strong>
-            {comment.text}
-            <button className="remove-comments">&times;</button>
-          </p>
-        </div>
-      );
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comments">&times;</button>
+        </p>
+      </div>
+    );
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    console.log(postId, author, comment);
   },
   render() {
     return (
       <div className="comment">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author"></input>
           <input type="text" ref="comment" placeholder="comment"></input>
-          <input type="submit" hidden/>
+          <input type="submit" hidden />
         </form>
       </div>
     );
